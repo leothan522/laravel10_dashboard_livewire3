662dc503efb6726811bda521e98f06b0796f4a4e
Yonathan Castillo <leothan522@gmail.com>
2024-02-13 21:13:40 -0400

    Correciones en CHAT y FMC

--- app/Livewire/FCM/FcmComponent.php
+++ app/Livewire/FCM/FcmComponent.php
@@ -9,15 +9,12 @@ use Kreait\Firebase\Exception\FirebaseException;
 use Kreait\Firebase\Exception\MessagingException;
 use Kreait\Firebase\Messaging\CloudMessage;
 use Kreait\Firebase\Messaging\Notification;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class FcmComponent extends Component
 {
     use LivewireAlert;
-
-    protected $listeners = [
-        'tokenSeleccionado'
-    ];
 
     public $title, $body, $fcm_token = "todos", $fcm_tipo;
     private $messaging;
@@ -99,10 +96,9 @@ class FcmComponent extends Component
         }
     }
 
+    #[On('tokenSeleccionado')]
     public function tokenSeleccionado($token)
     {
         $this->fcm_token = $token;
     }
-
-
 }
